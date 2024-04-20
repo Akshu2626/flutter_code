6e5e0e84d213dcaee45e855f2505852e2ea2b63a
akshu2626 <akshugajbhiye97@gmail.com>
2024-04-21 00:07:29 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('youtube'),
+      ),
+    );
+  }
+}
