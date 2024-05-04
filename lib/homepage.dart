d074452fc9132d2e101e32afde1bf2eb5c2f7fe6
akshu2626 <akshugajbhiye97@gmail.com>
2024-05-05 01:26:20 +0530

    i add backcolor in master

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatefulWidget {
+class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Homepage'),),
+      backgroundColor: Colors.purple.shade200,
     );
   }
 }
